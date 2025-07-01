e9a8eb4a273a63badc4d760d60d4db4eed190036
Franccesco Petta <franccescopettamuller@gmail.com>
2025-07-01 15:54:01 -0400

    good starting point

---
+++ src/components/CampusDetails.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CampusDetails = () => {
+    return (
+        <div>
+            <h1>Campus Details</h1>
+        </div>
+    )
+}
+
+export default CampusDetails;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./NavBarStyles.css";
-
+import { NavLink } from "react-router";
+ 
 const NavBar = () => {
-  return <nav className="navbar">{/* Some navbar stuff goes here */}</nav>;
+  return (
+    <nav className="navbar">
+      <NavLink to = "/">🏠</NavLink>
+      <NavLink to = "/campuses">All Campuses</NavLink>
+      <NavLink to ="/students">All Students</NavLink>
+    </nav>
+  );
 };
 
 export default NavBar;
