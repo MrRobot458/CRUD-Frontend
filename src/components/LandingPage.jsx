85493fabe53f03e760a4199c48fdde347a6e3be1
Franccesco Petta <franccescopettamuller@gmail.com>
2025-07-05 17:07:15 -0400

    added links in the landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import "./LandingPage.css";
+import { Link } from "react-router";
 
 const LandingPage = () => {
   return (
     <div className="landing-page">
       <div className="landing-bubble">
         <h1 className="landing-title">Welcome to CampusConnect</h1>
-        <p>Explore campuses. Connect with students. Start your journey.</p>
+        <h3>Start your journey</h3>
+        <Link to="/campuses">
+          <p>Explore campuses</p>
+        </Link>
+        <Link to="/students">
+          <p>Connect with students</p>
+        </Link>
       </div>
 
       <div className="left-align-wrapper">
@@ -16,7 +23,6 @@ const LandingPage = () => {
       </div>
     </div>
   );
-
 };
 
 export default LandingPage;
